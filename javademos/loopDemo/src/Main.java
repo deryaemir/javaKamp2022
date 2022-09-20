dabe4ec792b527763ee5f0ef0e74a6384f5b0d48
deryaemir <113916606+deryaemir@users.noreply.github.com>
2022-09-20 23:25:04 +0300

    Add files via upload

---
+++ javademos/loopDemo/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+	//for
+        for(int i=2;i<10;i+=2){
+            System.out.println(i);
+        }
+        System.out.println("For döngüsü bitti.");
+        //Bir değişken sadece tanımlandığı yerde (blokta) geçerlidir.
+    //while
+        int i=2;
+        while (i<10){
+            System.out.println(i);
+            i+=2;
+        }
+        System.out.println("While döngüsü bitti.");
+    //Do-While
+        int j=1;
+        do{
+            System.out.println(j);
+            j+=2;
+        }while (j<10);
+        System.out.println("Do-while döngüsü bitti.");
+    }
+}
---
+++ javademos/variables/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+	//reusability(tekrar tekrar kullanım)
+        //değişkenleri bellekten tasarruf yapmak ve tekrar tekrar kullanabilmek için kullanıyoruz.
+        String mesaj="Öğrenci sayım:";
+        int ogrenciSayisi=12;
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println("Öğrenci Sayım:"+ogrenciSayisi);
+        System.out.println("Öğrenci Sayım:"+ogrenciSayisi);
+    }
+}
